9a87ad7df08b42c3df8ee7ffca5317d6f684b417
Benjamin Kampmann <ben@gnunicorn.org>
2025-07-31 20:04:57 +0100

    Cleaning up dependencies and old blob store code

--- crates/blob-store/src/main.rs
+++
@@ -1,8 +0,0 @@
-use zoe_blob_store::server;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Run the server with command line arguments
-    server::run_with_args().await?;
-    Ok(())
-}
