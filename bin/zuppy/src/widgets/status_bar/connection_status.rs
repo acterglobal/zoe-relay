7bca350539d44f1e6c1379979d1d74930559ca03
Benjamin Kampmann <ben@gnunicorn.org>
2025-10-21 13:56:14 +0100

    task listener added. cleared into simpler entity

--- bin/zuppy/src/widgets/status_bar/connection_status.rs
+++ bin/zuppy/src/widgets/status_bar/connection_status.rs
@@ -1,70 +1,96 @@
 use futures::{StreamExt, pin_mut};
 use gpui::{
-    AppContext, Context, Entity, InteractiveElement, IntoElement, ParentElement, Render,
-    SharedString, StatefulInteractiveElement, Styled, Subscription, Window, div,
+    AppContext, AsyncApp, Context, Entity, InteractiveElement, IntoElement, ParentElement, Render,
+    SharedString, StatefulInteractiveElement, Styled, Subscription, Task, WeakEntity, Window, div,
 };
 use zoe_client::OverallConnectionStatus;
 
 use crate::{ClientState, widgets::simple_popover::SimplePopover};
 
-pub struct ConnectionStatus {
-    client_state: Entity<ClientState>,
-    current_status: Entity<Option<OverallConnectionStatus>>,
+struct ConnectionInfoInner {
     _client_subscription: Subscription,
+    current_task: Option<Task<()>>,
+    current_state: Option<OverallConnectionStatus>,
 }
 
-fn update_of_client_state(
-    cx: &mut Context<ConnectionStatus>,
-    client_state: Entity<ClientState>,
-    current_status: Entity<Option<OverallConnectionStatus>>,
-) {
-    let zoe = if let ClientState::Zoe(z) = client_state.read(cx) {
-        tracing::info!("found a client");
-        z.clone()
-    } else {
-        tracing::info!("no client");
-        current_status.update(cx, |_, _| Option::<OverallConnectionStatus>::None);
-        return;
-    };
+impl ConnectionInfoInner {
+    fn new(client_state: Entity<ClientState>, cx: &mut Context<Self>) -> Self {
+        let mut s = Self {
+            _client_subscription: cx.observe(&client_state, Self::update_of_client_state),
+            current_state: None,
+            current_task: None,
+        };
+        s.update_of_client_state(client_state, cx);
+        s
+    }
 
-    cx.spawn(async move |_, ctx| {
-        let new_status = zoe.overall_status().await;
-        tracing::info!("gotten status: {new_status:?}");
-        if let Err(e) = current_status.update(ctx, |m, c| {
-            *m = Some(new_status);
-            c.notify();
-        }) {
-            tracing::warn!("Updating current status failed: {e}");
-        }
-        tracing::info!("updated status");
-        // gpui_tokio::Tokio::spawn(ctx, async move {
-        let stream = zoe.overall_status_stream();
-        pin_mut!(stream);
-        while let Some(n) = stream.next().await {
-            if let Err(e) = current_status.update(ctx, |m, c| {
-                *m = Some(n);
-                c.notify();
-            }) {
-                tracing::warn!("Updating current status failed: {e}");
+    fn update_of_client_state(
+        &mut self,
+        client_state: Entity<ClientState>,
+        cx: &mut Context<Self>,
+    ) {
+        let ClientState::Zoe(z) = client_state.read(cx) else {
+            tracing::info!("no client");
+            let mut should_notify = self.current_state.take().is_some();
+            if let Some(t) = self.current_task.take() {
+                should_notify = true;
+                drop(t);
+            }
+            if should_notify {
+                cx.notify();
+            }
+            return;
+        };
+
+        let zoe = z.clone();
+
+        self.current_task = Some(cx.spawn(async move |w, ctx| {
+            if !Self::update_state_info(&w, zoe.overall_status().await, ctx) {
                 return;
             }
+            let stream = zoe.overall_status_stream();
+            pin_mut!(stream);
+            while let Some(n) = stream.next().await {
+                if !Self::update_state_info(&w, n, ctx) {
+                    return;
+                }
+            }
+        }));
+    }
+
+    fn update_state_info(
+        w: &WeakEntity<ConnectionInfoInner>,
+        new_status: OverallConnectionStatus,
+        ctx: &mut AsyncApp,
+    ) -> bool {
+        tracing::info!("gotten status: {new_status:?}");
+        let Some(current_status) = w.upgrade() else {
+            tracing::info!("entity is already gone");
+            return false;
+        };
+
+        if let Err(e) = current_status.update(ctx, |m, c| {
+            m.current_state = Some(new_status);
+            c.notify();
+        }) {
+            tracing::warn!("Updating connection info status failed: {e}");
+            return false;
         }
-    })
-    .detach(); // forever run in background
+        tracing::info!("updated status");
+        return true;
+    }
+}
+
+pub struct ConnectionStatus {
+    client_state: Entity<ClientState>,
+    current_status: Entity<ConnectionInfoInner>,
 }
 
 impl ConnectionStatus {
     pub fn new(client_state: Entity<ClientState>, cx: &mut Context<Self>) -> Self {
-        let current_status = cx.new(|_| None);
-        let current_status_ref = current_status.clone();
-        let client_sub = cx.observe(&client_state, move |_s, n, cx| {
-            update_of_client_state(cx, n, current_status_ref.clone())
-        });
-        update_of_client_state(cx, client_state.clone(), current_status.clone()); // call once to initialize
         Self {
-            client_state,
-            current_status,
-            _client_subscription: client_sub,
+            client_state: client_state.clone(),
+            current_status: cx.new(|cx| ConnectionInfoInner::new(client_state, cx)),
         }
     }
 }
@@ -85,7 +111,7 @@ impl ConnectionStatus {
         let e = div()
             .id("connected_info")
             .child(SharedString::new_static("Connected"));
-        if let Some(info) = self.current_status.read(cx) {
+        if let Some(ref info) = self.current_status.read(cx).current_state {
             let msg = format!("{} / {}", info.connected_count, info.total_count);
             return e.cursor_pointer().hoverable_tooltip(move |_w, ctx| {
                 ctx.new(|_| SimplePopover::new(msg.clone().into())).into()
